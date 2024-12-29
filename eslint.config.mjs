879f9af33eea1e88d8d39d3bff7e3f4ab628aafc
Jordan Hofker <jordanho@zillowgroup.com>
2024-12-28 22:48:17 -0600

    lint command changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,30 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/scripts/**",
+      "**/*.js",
+      "**/*.jsx",
+      "**/*.mjs",
+      "**/*.cjs"
+    ]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    rules: {},
+  }
 ];
 
 export default eslintConfig;
