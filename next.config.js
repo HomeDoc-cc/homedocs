0dfeae73915531f8ce1deafda4135ba25f7f794c
Jordan Hofker <jhofker@gmail.com>
2025-01-15 15:48:30 -0600

    try to add cors headers

--- next.config.js
+++ next.config.js
@@ -16,6 +16,28 @@ const nextConfig = {
     ],
   },
   transpilePackages: ['libheif-js', 'heic-decode', 'heic-convert'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              img-src 'self' data: https://*.googleapis.com https://*.gstatic.com;
+              font-src 'self' https://fonts.gstatic.com;
+              connect-src 'self' https://*.googleapis.com https://*.gstatic.com;
+            `
+              .replace(/\s+/g, ' ')
+              .trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
